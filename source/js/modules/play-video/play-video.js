f32869885cc92c4d4a0a9927464a60201e351204
eugeneps <blaze97197@gmail.com>
2023-11-02 23:47:09 +0700

    Добавляет стили и js для блока presentation (десктоп и планшет)

---
+++ source/js/modules/play-video/play-video.js
@@ -0,0 +1,18 @@
+const playBtn = document.querySelector('.presentation__play-button');
+const playerWrapper = document.querySelector('.presentation__video');
+const player = playerWrapper.querySelector('iframe');
+const poster = playerWrapper.querySelector('img');
+
+
+
+const startVideo = () => {
+  playBtn.addEventListener('click', function () {
+    poster.style.display = 'none';
+    playBtn.style.display = 'none';
+    playerWrapper.classList.remove('presentation__video--overlay');
+    player.style.display = 'block';
+    player.src += '&autoplay=1';
+  });
+};
+
+export {startVideo};
