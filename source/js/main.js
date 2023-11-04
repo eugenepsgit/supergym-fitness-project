37a9904a30a19dbf9d4b6720efc2f1ca42e0b30f
eugeneps <blaze97197@gmail.com>
2023-11-04 23:15:00 +0700

    завершает стилизацию блока tariff и подключает js для работы табов

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {startVideo} from './modules/play-video/play-video';
+import {initTabs} from './modules/tabs/init-tabs';
 
 // ---------------------------------
 
@@ -23,6 +24,7 @@ window.addEventListener('DOMContentLoaded', () => {
     window.form = form;
     form.init();
     startVideo();
+    initTabs();
   });
 });
 
